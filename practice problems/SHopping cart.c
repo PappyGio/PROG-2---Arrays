d6e6c86915c06dd0b3d31e8cdf1fb0b67c5b4a21
Coding of Geograpikals chanel <161205801+PappyGio@users.noreply.github.com>
2024-06-18 07:44:00 +0800

    Update SHopping cart.c

--- practice problems/SHopping cart.c
+++ practice problems/SHopping cart.c
@@ -11,23 +11,24 @@
 
 //arrays to be made
 //1, display stock 
-//2. remove cart
-//3. view cart 
-//4. check out
-//5. add to cart 
-//6. 
+//2. update stock
+//3. change price
+//4. add to cart
+//5. Remove cart 
+//6. view cart
+//7.check out 
 #include <stdio.h>
 
 void displaystock(char name[][50], int quantity[], float price[], int size) {
-    printf("\nIndex\tName\t\tQuantity\tPrice\n");
+    printf("\nIndex\tName\t\t\tQuantity\tPrice\n");
     printf("----------------------------------------------\n");
     for (int i = 0; i < size; i++) {
         printf("%d\t%s\t\t%d\t\t$%.2f\n", i + 1, name[i], quantity[i], price[i]);
     }
 }
 
-void updatestock(char name[][50], int quantity[], int index, int newquantity) {
-    if (index >= 1 && index <= sizeof(name) / sizeof(name[0])) {
+void updatestock(char name[][50], int quantity[], int size, int index, int newquantity) {
+    if (index >= 1 && index <= size) {
         quantity[index - 1] = newquantity;
         printf("Quantity of %s updated to %d.\n", name[index - 1], newquantity);
     } else {
@@ -35,8 +36,16 @@ void updatestock(char name[][50], int quantity[], int index, int newquantity) {
     }
 }
 
+void changeprice(char name[][50], float price[], int size, int index, float newprice){
+      if(index >= 1 && index <= size){
+        price[index - 1] = newprice;
+        printf("Price of %s updated to $%.2f.\n", name[index-1], newprice);
+      }else{
+        printf("Invalid index.\n");  
+      }
+}
 
-void changeprice(){
+void addtocart(){
     
 }
 
@@ -44,7 +53,7 @@ int main() {
     char name[][50] = {"Milk Choc", "Dark Choc", "Mint Chocolate", "White Choc", "Peanut Choc"};
     int quantity[] = {60, 42, 29, 60, 38};
     float price[] = {12.00, 25.00, 20.00, 15.00, 8.00};
-    int size = sizeof(name) / sizeof(name[0]); // assuming all arrays have the same size
+    int size = sizeof(quantity) / sizeof(quantity[0]); // assuming all arrays have the same size
 
     char choice;
     printf("Do you want to display the stock? (y/n): ");
@@ -59,14 +68,33 @@ int main() {
     int index, newquantity;
     printf("Enter the index of the item you want to update: ");
     scanf("%d", &index);
-    printf("Enter the new quantity for %s: ", name[index - 1]);
-    scanf("%d", &newquantity);
-
-    updatestock(name, quantity, index, newquantity);
+    if (index >= 1 && index <= size) {
+        printf("Enter the new quantity for %s: ", name[index - 1]);
+        scanf("%d", &newquantity);
+        updatestock(name, quantity, size, index, newquantity);
+    } else {
+        printf("Invalid index.\n");
+    }
 
     // Display updated stock
     printf("\nUpdated Stock:\n");
     displaystock(name, quantity, price, size);
 
+
+
+    //change price of an item 
+    float newprice;        
+    printf("Enter the index of the item whose price you want to update: ");
+    scanf("%d", &index);
+    if (index >= 1 && index <= size) {
+        printf("Enter the new price for %s: ", name[index - 1]);
+        scanf("%f", &newprice);
+        changeprice(name, price, size, index, newprice);
+    } else {
+        printf("Invalid index.\n");
+    }
+
+
     return 0;
 }
+
