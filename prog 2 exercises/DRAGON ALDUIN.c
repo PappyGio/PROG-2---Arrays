c00ca7dc436318c8a39f747837e0b6ff120dc064
Coding of Geograpikals chanel <161205801+PappyGio@users.noreply.github.com>
2024-06-21 15:51:38 +0800

    Create DRAGON ALDUIN.c

---
+++ prog 2 exercises/DRAGON ALDUIN.c
@@ -0,0 +1,26 @@
+/* 
+PROBLEM: 
+
+
+str1           Dreaded doom darkening skies above 
+str2           Ravenous red racen a rising legend 
+str 3          Ancient Arcane creature of great destruction
+str 4          Great Greedy source of upheavel
+str 5          Ominous Winged Overlord with scales of iron 
+str 6          Nightmare of the realm and devourer of nations 
+
+
+
+DRAGIB ALDUIN 
+
+Diamonds : number of wordss
+Rubies :  # of r 
+Emeralds: #of words with letters < 4
+Gold: # of vowels
+Silver: #of consenants
+Copper: # of spaces 
+
+
+Make a function based on these conditions 
+
+*/
