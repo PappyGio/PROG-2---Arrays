09f5e6f9265f3d31af1c3e8399fa3861cafd65fa
Coding of Geograpikals chanel <161205801+PappyGio@users.noreply.github.com>
2024-06-28 14:59:44 +0800

    Update sentence palindrome.c

--- practice problems 2/sentence palindrome.c
+++ practice problems 2/sentence palindrome.c
@@ -1,3 +1,9 @@
+/* FUNCTION SPECIFICATION:
+Create a program & a function that inputs 
+a user a sentence and  finds the palindrome in the sentence 
+*/
+
+
 #include<stdio.h>
 #include<string.h>
 #include<ctype.h>
@@ -18,6 +24,7 @@ int ispalindrome(char *str){
 void findpalindrome(char *sentence){
     char word[MAX_LEN];
     int wordindex = 0;
+    int palindromefound = 0; // flag top track if palindrome is found 
     for(int i = 0; sentence[i] !='\0'; i++){
         char c = sentence[i];
 
@@ -27,6 +34,7 @@ void findpalindrome(char *sentence){
             word[wordindex] = '\0';
             if(ispalindrome(word)){
                 printf("Palindrome found: %s\n", word);
+                palindromefound = 1;
             }
             wordindex = 0;
         }
@@ -39,6 +47,9 @@ void findpalindrome(char *sentence){
             if(ispalindrome(word)){
               printf("Palindrome found: %s\n", word);  
             }
+    }
+    if(!palindromefound){
+        printf("No palindrome found ");
     }
 
 }
